8ba14de8519ed71d62b736c6c1fb8098afd53dd9
jenhsiaoman <2831672553@qq.com>
2025-07-21 16:16:04 +0800

    Clean up files ignored by .gitignore

--- train.sh
+++
@@ -1,6 +0,0 @@
-python train.py \
- --batch_size 2 \
- --num_workers 4 \
- --task_name "Task2203_picai" \
- --checkpoint "ckpt/sam_med3d_turbo.pth" \
- --lr 8e-5
--- val_on_dataset.sh
+++
@@ -1,5 +0,0 @@
-python medim_val_dataset.py
-
-python utils/compute_dataset_metrics.py \
-    --gt_dir data/Task204_picai_lesion/labelsTr \
-    --pred_dir data/Task204_picai_lesion/pred_sammed3d
